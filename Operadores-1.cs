55ad7b86cdfd6a3c2066bfa856cc7d2331478809
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 11:02:35 -0300

    Add files via upload

---
+++ Operadores-1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite a idade do usuario: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        Console.Write("Possui carteira de motorista? (sim/não): ");
+        string resposta = Console.ReadLine().ToLower().Trim();
+
+        if (idade > 18 && resposta == "sim")
+        {
+            Console.WriteLine("Voce pode dirigir: ");
+        }
+
+    }
+}
