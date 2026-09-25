ef6cf40b5538470709884305b10b07032db9f499
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 21:28:24 -0300

    Add files via upload

---
+++ Operadores-4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("É assinante do serviço de streaming? (sim/nao): ");
+        string assinante = Console.ReadLine().ToLower().Trim();
+
+        Console.Write("Já assistiu à série específica? (sim/nao): ");
+        string assistiu = Console.ReadLine().ToLower().Trim();
+
+        if (assinante == "sim" && assistiu == "sim")
+        {
+            Console.WriteLine("Otimo, agora voce pode assitir a nova temporada! ");
+        }
+    }
+}
---
+++ Operadores_5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite o seu nome: ");
+        string nome = Console.ReadLine();
+
+        Console.Write("Digite a sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (nome.Length > 3 && idade >= 18)
+        {
+            Console.WriteLine("Acesso permitido");
+        } else
+        {
+            Console.WriteLine("Acesso negado");
+        }
+    }
+}
