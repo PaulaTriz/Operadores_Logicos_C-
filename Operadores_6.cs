3332d380d0b9d7b6d994cff40a2fc00fae4aac97
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 21:37:13 -0300

    Add files via upload

---
+++ Operadores_6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Deseja adicionar açúcar? (sim/nao): ");
+        string acucar = Console.ReadLine().ToLower().Trim();
+
+        Console.Write("Deseja adicionar leite? (sim/nao): ");
+        string leite = Console.ReadLine().ToLower().Trim();
+
+        if (acucar == "sim" || leite == "sim")
+        {
+            Console.WriteLine("Cafe com adicional preparado!");
+        }
+    }
+}
---
+++ Operadores_7.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Digite um número inteiro: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        if (numero % 3 == 0 || numero % 5 == 0)
+        {
+            Console.WriteLine("O número é divisível por 3 ou por 5!");
+        }
+    }
+}
