f56b207154ee8298ac3ea795ebfa34e84292a559
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 21:05:01 -0300

    Add files via upload

---
+++ Operadores_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Deseja comprar um carro novo? (sim/nao): ");
+        string querComprar = Console.ReadLine().ToLower().Trim();
+
+        Console.Write("Tem dinheiro suficiente para isso? (sim/nao): ");
+        string temDinheiro = Console.ReadLine().ToLower().Trim();
+
+        if (querComprar == "sim" && temDinheiro == "sim")
+        {
+            Console.WriteLine("Parabens, voce pode comprar um carro novo! ");
+        }
+    }
+}
---
+++ Operadores_3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Possui diploma de ensino superior? (sim/nao): ");
+        string diploma = Console.ReadLine().ToLower().Trim();
+
+        Console.Write("Digite a sua idade: ");
+        int idade = int.Parse(Console.ReadLine());
+
+        if (diploma == "sim" && idade > 21)
+        {
+            Console.WriteLine("Parabens, voce esta qualificado para a vaga! ");
+        }
+    }
+}
+
+
