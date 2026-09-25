41f2202a5dfe227d6dfaead18fde2703a92ae19e
Paula Rodrigues <rodriguespereirapaula649@gmail.com>
2026-09-24 21:44:35 -0300

    Add files via upload

---
+++ Operadores_8.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Deseja comprar um produto? (sim/s/nao): ");
+        string resposta = Console.ReadLine().ToLower().Trim();
+
+        if (resposta == "sim" || resposta == "s")
+        {
+            Console.WriteLine("Obrigado pela compra!");
+        }
+    }
+}
